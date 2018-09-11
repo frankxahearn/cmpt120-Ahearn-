f74032dc4ebd7ea3cda2397c7defd0472af9e321
Frank Ahearn <frank.ahearn1@marist.edu>
2018-09-11 17:50:03 -0400

    Solution to Mad Libs exercise

---
+++ madlib.py
@@ -0,0 +1,26 @@
+#Frank Ahearn
+#9/11/2018
+#madlibs.py
+
+def main():
+    
+    print("Mad Libs")
+    print()
+    
+    noun = input("Enter a noun:")
+    verb = input("Enter a verb:")
+    adjective = input("Enter a adjective:")
+    place = input("Enter a place:")
+
+    print ("This is a story about the" , noun, " that lived in a" , place,
+           "who liked to go to the beach and", verb, "wear a", adjective, " hat")
+    
+
+    
+
+
+
+    
+
+main()
+    
